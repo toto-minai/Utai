93f441d722edbf300bd2bcb5b75238baf578e6fd
Toto Minai <bufhdy@hotmail.com>
2021-06-09 20:48:09 +0900

    Initial Commit

---
+++ Utai/UtaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtaiApp.swift
+//  Utai
+//
+//  Created by Toto Minai on 2021/06/09.
+//
+
+import SwiftUI
+
+@main
+struct UtaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
