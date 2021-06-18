4afa354394c7f2a0f74b0aa76602bfdafcd93aac
Toto Minai <bufhdy@hotmail.com>
2021-06-18 10:39:24 +0900

    Correct optional var for JSON parser

--- Utai/Model/MatchSearchResult.swift
+++ Utai/Model/MatchSearchResult.swift
@@ -45,7 +45,7 @@ struct MatchSearchResult: Codable {
     struct Format: Codable {
         let name: String
         let qty: String?
-        let descriptions: [String]
+        let descriptions: [String]?
     }
 
     struct Track: Codable {
