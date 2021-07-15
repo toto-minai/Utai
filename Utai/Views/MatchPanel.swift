a6037999e4395482c7d98617b8b8260ea06eb717
Toto Minai <bufhdy@hotmail.com>
2021-07-15 17:15:40 +0900

    Avoid necessary copying of list items

--- Utai/Views/MatchPanel.swift
+++ Utai/Views/MatchPanel.swift
@@ -293,8 +293,7 @@ extension MatchPanel {
         withAnimation(.spring()) {
             DispatchQueue.main.async {
                 mismatchedTracks.removeAll { $0.id == localTrack.id }
-                // Can only use a copy or it will generate extra spaces in Xcode 13 Beta 1
-                matchedTracks.append(localTrack.copy())
+                matchedTracks.append(localTrack)
             }
         }
     }
